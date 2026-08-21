71a979187eb2c882daf4a830c5e04887362c9b7a
jkibura <167850144+jkibura@users.noreply.github.com>
2026-08-21 12:08:45 +0300

    Fixed hero image problem by making hero section a flex, adding a child div below it and centering it. Tried to update text-size responsivity but I'll do that later. I'll need to redesign the styling architecture to rely 100% on variables.

--- src/components/sections/Hero/Hero.tsx
+++ src/components/sections/Hero/Hero.tsx
@@ -9,29 +9,32 @@ function Hero({ content }: HeroProps) {
   return (
     <section id="home" className={styles.hero}>
       <div className={styles['hero-content']}>
-        <p className={styles['eyebrow']}>{content.eyebrow}</p>
+        <div className={styles['hero-text']}>
+          <p className={styles['eyebrow']}>{content.eyebrow}</p>
 
-        <h1 className={styles['title']}>
-          {content.title}{' '}
-          <span className={styles['highlight']}>{content.highlight}</span>
-        </h1>
+          <h1 className={styles['title']}>
+            {content.title}{' '}
+            <span className={styles['highlight']}>{content.highlight}</span>
+          </h1>
 
-        {content.subtitle && <p className={styles['subtitle']}>{content.subtitle}</p>}
+          {content.subtitle && <p className={styles['subtitle']}>{content.subtitle}</p>}
 
-        <div className={styles['cta-group']}>
-          <a className={styles['cta-pri']} href={content.primaryCta.href}>
-            {content.primaryCta.label}
-          </a>
-          {content.secondaryCta && (
-            <a className={styles['cta-sec']} href={content.secondaryCta.href}>
-              {content.secondaryCta.label}
+          <div className={styles['cta-group']}>
+            <a className={styles['cta-pri']} href={content.primaryCta.href}>
+              {content.primaryCta.label}
             </a>
-          )}
+            {content.secondaryCta && (
+              <a className={styles['cta-sec']} href={content.secondaryCta.href}>
+                {content.secondaryCta.label}
+              </a>
+            )}
+          </div>
         </div>
-      </div>
-
-      <div className={styles['image']}>
-        {content.image && <img src={content.image?.src} alt={content.image?.alt} />}
+      
+        <div className={styles['image']}>
+          {content.image && <img src={content.image?.src} alt={content.image?.alt} />}
+        </div>
+        
       </div>
     </section>
   )
